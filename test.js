f3a93766c5c09db0c5424b0dc6f0416da55483f3
Jasmine <jasmine-aips@hotmail.com>
2020-01-23 14:43:59 +0800

    need to optimise to solve last one

--- test.js
+++ test.js
@@ -1,22 +1,33 @@
-function sumSquares(n) {
-	let sum = 0;
-	for (let i = 1; i <= n; i++) {
-		sum += Math.pow(i, 2);
+// find all prime numbers until n
+
+function isPrime(number) {
+	let i = 2;
+	while (i < number) {
+		// console.log(`${number} / ${i} = ${number / i}`);
+		if (Number.isInteger(number / i)) {
+			return false;
+		} else if (i === number - 1) {
+			return true;
+		}
+		i++;
 	}
-	return sum;
 }
 
-function squaredSum(n) {
-	let sum = 0;
-	for (let i = 1; i <= n; i++) {
-		sum += i;
+function nthPrime(n) {
+	let primes = [ 2 ];
+	let number = 3;
+	while (primes.length < n) {
+		// while (number < 50) {
+		if (isPrime(number)) {
+			primes.push(number);
+			console.log(number, ' is a prime');
+		}
+		number += 2;
 	}
-	return Math.pow(sum, 2);
+	console.log(primes);
+	console.log(primes[n - 1]);
+	return primes[n - 1];
 }
 
-function sumSquareDifference(n) {
-	console.log(sumSquares(n) - squaredSum(n));
-	return Math.abs(sumSquares(n) - squaredSum(n));
-}
-
-sumSquareDifference(100);
+nthPrime(10);
+// should be 29
